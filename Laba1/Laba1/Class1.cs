5af214210a084c094236e2c2156fe654b90924be
Андрей Сипович <Andrey6618@outlook.com>
2020-03-23 18:30:33 +0300

    ytfyyffy

---
+++ Laba1/Laba1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba1
+{
+    class Class1
+    {
+
+        public string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int kol;
+        public int Kol
+        {
+            get { return kol; }
+            set { kol = value; }
+
+        }
+        public float stoimost;
+        public float Stoimost
+        {
+            get { return stoimost; }
+            set { stoimost = value; }
+
+        }
+
+
+
+        public void zap()
+        {
+            Console.WriteLine("Наименование: ");
+            name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Количество: ");
+            kol = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Стоимость: ");
+            stoimost = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void viv()
+        {
+            Console.WriteLine("Наименование: " + Name);
+            Console.WriteLine("Количество: " + Kol);
+            Console.WriteLine("Стоимость: " + Stoimost);
+        }
+
+
+
+    }
+}
---
+++ Laba1/Laba1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Laba1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Tovar = new Class1();
+            Tovar.zap();
+            Tovar.viv();
+        }
+
+    }
+}
